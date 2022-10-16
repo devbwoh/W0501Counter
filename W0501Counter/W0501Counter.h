3038de8ae45feca6b66af37b8a3023ca60042fe7
BWOH <bwoh@outlook.kr>
2022-10-17 00:03:58 +0900

    초기 커밋.

---
+++ W0501Counter/W0501Counter.h
@@ -0,0 +1,32 @@
+﻿
+// W0501Counter.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CW0501CounterApp:
+// 이 클래스의 구현에 대해서는 W0501Counter.cpp을(를) 참조하세요.
+//
+
+class CW0501CounterApp : public CWinApp
+{
+public:
+	CW0501CounterApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CW0501CounterApp theApp;
---
+++ W0501Counter/W0501CounterDlg.h
@@ -0,0 +1,43 @@
+﻿
+// W0501CounterDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CW0501CounterDlg 대화 상자
+class CW0501CounterDlg : public CDialogEx
+{
+protected:
+	int CountValue = 0;
+	CString txtCount;
+// 생성입니다.
+public:
+	CW0501CounterDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_W0501COUNTER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnAdd();
+protected:
+	int editValue;
+public:
+	afx_msg void OnSet();
+};
